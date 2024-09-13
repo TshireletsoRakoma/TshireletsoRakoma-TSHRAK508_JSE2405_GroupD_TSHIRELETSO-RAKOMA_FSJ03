75e1c2779f5e7b59d5a2fce2948f00f6fc184d27
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-09-13 13:13:47 +0200

    Add JSDoc comments to `ErrorBoundary` component
    - Added JSDoc comments to `ErrorBoundary` for improved documentation.
    - Described the component's purpose, props, and error handling mechanism.

--- app/Components/ErrorBoundary.js
+++ app/Components/ErrorBoundary.js
@@ -1,16 +1,32 @@
-// components/ErrorBoundary.js
 'use client';
 import { useState } from 'react';
 
+/**
+ * ErrorBoundary component catches JavaScript errors anywhere in its child component tree 
+ * and displays a fallback UI instead of crashing the whole app.
+ * 
+ * @function ErrorBoundary
+ * @param {Object} props - The component props.
+ * @param {JSX.Element} props.children - The children components to render.
+ * @param {JSX.Element} props.fallback - The fallback UI to display when an error occurs.
+ * @returns {JSX.Element} The component rendering its children or the fallback UI if an error occurs.
+ */
 export default function ErrorBoundary({ children, fallback }) {
   const [hasError, setHasError] = useState(false);
+
+  /**
+   * Error handling callback to set the error state.
+   */
+  const handleError = () => {
+    setHasError(true);
+  };
 
   if (hasError) {
     return fallback;
   }
 
   return (
-    <div onError={() => setHasError(true)}>
+    <div onError={handleError}>
       {children}
     </div>
   );
