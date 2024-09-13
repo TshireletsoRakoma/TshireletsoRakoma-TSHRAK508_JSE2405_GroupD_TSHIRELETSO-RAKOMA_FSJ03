f9eb7c3cdfe6e6eff7691260d916bb0058333b6f
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-09-13 12:59:18 +0200

    Add JSDoc comments to `ProductReviews` component
    - Added JSDoc comments to `ProductReviews` for enhanced code documentation.
    - Described the parameters and return value of the component.

--- app/Components/ProductReviews.js
+++ app/Components/ProductReviews.js
@@ -1,23 +1,35 @@
+'use client';
 
-'use clent'; 
+/**
+ * ProductReviews component displays a list of product reviews.
+ * 
+ * @function ProductReviews
+ * @param {Object[]} reviews - Array of review objects.
+ * @param {string} reviews[].id - Unique identifier for the review.
+ * @param {string} reviews[].name - Name of the reviewer.
+ * @param {number} reviews[].rating - Rating given by the reviewer (out of 5).
+ * @param {string} reviews[].createdAt - Date when the review was created (ISO string).
+ * @param {string} reviews[].comment - Review comment text.
+ * @returns {JSX.Element} The component rendering the list of reviews or a message if no reviews are available.
+ */
 export default function ProductReviews({ reviews }) {
-    return (
-      <div>
-        <h2 className="text-2xl font-bold mb-4">Reviews</h2>
-        {reviews.length > 0 ? (
-          <div className="space-y-4">
-            {reviews.map((review) => (
-              <div key={review.id} className="border p-4 rounded-lg shadow-md">
-                <h3 className="text-xl font-bold">{review.name}</h3>
-                <p className="text-gray-600">Rating: {review.rating}/5</p>
-                <p className="text-gray-600">Date: {new Date(review.createdAt).toLocaleDateString()}</p>
-                <p>{review.comment}</p>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>No reviews yet.</p>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Reviews</h2>
+      {reviews.length > 0 ? (
+        <div className="space-y-4">
+          {reviews.map((review) => (
+            <div key={review.id} className="border p-4 rounded-lg shadow-md">
+              <h3 className="text-xl font-bold">{review.name}</h3>
+              <p className="text-gray-600">Rating: {review.rating}/5</p>
+              <p className="text-gray-600">Date: {new Date(review.createdAt).toLocaleDateString()}</p>
+              <p>{review.comment}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No reviews yet.</p>
+      )}
+    </div>
+  );
+}
